a74ae762c205f2c36df5a5c055c728d9566cb22f
Jaco <jaco.duplessis@uqconnect.edu.au>
2021-09-05 19:26:16 +1000

    dockerfile and api

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,27 @@
+FROM golang:1.17-alpine as builder
+WORKDIR /work
+
+# enable gin gonic relase mode
+ENV GIN_MODE=release
+
+# add files
+ADD api/ .
+# compile app static
+RUN go get ./...
+RUN \
+  CGO_ENABLED=0 \
+  GOOS=linux \
+  GOARCH=amd64 \
+  go build -a -installsuffix cgo -ldflags="-w -s" -o go-rtmp-api .
+
 FROM dvdgiessen/nginx-rtmp-docker
 
 RUN apk --no-cache add ffmpeg
 
+COPY --from=builder /work/go-rtmp-api /
+
 # Set up config file
 COPY conf/nginx.conf /etc/nginx/nginx.conf
 RUN chmod 444 /etc/nginx/nginx.conf
+
+
---
+++ api/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+
+	r.POST("/recorded", func(c *gin.Context) {
+		file_path := c.PostForm("path")
+		tcUrl := c.PostForm("tcUrl")
+		name := c.PostForm("name")
+		fmt.Printf(file_path, tcUrl, name)
+		c.String(http.StatusOK, "")
+
+		// TODO: Notify recording ready, and get presigned url
+
+		// Do upload
+
+		// Notify upload done
+
+	})
+
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
